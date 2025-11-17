9359095f863557a6ffec304996d434699da357a1
nicole <njl55@cornell.edu>
2025-11-17 17:56:21 -0500

    frontend intialized

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="h-screen w-full flex items-center justify-center bg-blue-100">
+      <h1 className="text-4xl font-bold text-blue-700">
+        Initialized frontend is working :D !
+      </h1>
+    </div>
+  );
+}
